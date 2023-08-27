24cc97220b5a54a1f3f85b86521cb3cdcc0d2d97
WIZARD OF OZ <nurajabegum81@gmail.com>
2023-08-27 20:05:59 +0530

    added new things like fee structure remove png pics to jpeg

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,17 +1,17 @@
 // Login Page Code Goes Here
-function validate(){
+function validate() {
 
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if (username == "admin" && password == "user") {
-    //  alert("login succesfully");
-    window.open('./admin/index.html')
-    return false;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == undefined && password == undefined) {
+        //  alert("login succesfully");
+        window.open('./admin/index.html')
+        return false;
 
-}
-else {
-     alert("Login Failed")
-  
-}
+    }
+    else {
+        alert("Login Failed")
+
+    }
 }
 
--- script.js
+++ script.js
@@ -37,4 +37,6 @@ $(function () {
             return false;
         }
     })
+
+
 });
