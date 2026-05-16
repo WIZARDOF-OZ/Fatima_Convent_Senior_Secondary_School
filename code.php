ff0c3ac2018079682e1be548d3d672929bb42576
WIZARD OF OZ <79371166+WIZARDOF-OZ@users.noreply.github.com>
2026-05-16 21:59:14 +0530

    Update recipient email address in code.php

--- code.php
+++ code.php
@@ -7,7 +7,7 @@
 $from = $_POST['email'];
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'nurajabegum81@gmail.com';
+$sendTo = 'XYZ@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
